ee3f59caf20afcc70ecbf596daaa0e93e83ff1c5
seen159-01 <shawnc5662@gmail.com>
2026-01-21 09:52:53 -0500

    remove clangd from wsl config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -13,17 +13,17 @@ vim.opt.scrolloff = 15
 
 
 --wsl clipboard management
-vim.g.clipboard = {
-
-    name = 'WslClipboard',
-    copy = {
-        ["+"] = 'clip.exe',
-        ["*"] = 'clip.exe',
-    },
-    paste = {
-        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-}
+-- vim.g.clipboard = {
+--
+--     name = 'WslClipboard',
+--     copy = {
+--         ["+"] = 'clip.exe',
+--         ["*"] = 'clip.exe',
+--     },
+--     paste = {
+--         ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--         ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     },
+--     cache_enabled = 0,
+-- }
 vim.opt.clipboard = "unnamedplus"
